3a9523f9f4baa5fdbfafbd06eb685afae9754402
My Dev Me <temasuperdev@gmail.com>
2026-04-01 22:20:01 +0300

    mini

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
+import socket
 from fastapi import FastAPI
 
-app = FastAPI(title="Simple App")
+app = FastAPI(title="Pod Info App")
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello from k3s with Traefik!"}
+def root():
+    hostname = socket.gethostname()
+    return {
+        "pod": hostname,
+        "message": "Hello from k3s with Traefik!"
+    }
+
+@app.get("/whoami")
+def whoami():
+    hostname = socket.gethostname()
+    return {
+        "pod": hostname,
+        "ip": socket.gethostbyname(hostname)
+    }
 
 @app.get("/health")
 def health():
